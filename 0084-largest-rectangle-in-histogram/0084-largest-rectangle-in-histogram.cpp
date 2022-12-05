9b3ebec0df0209c7d4c0dc55b215c5bba36ca7e0
Shourya Srivastava <shourya70073@gmail.com>
2022-12-05 18:45:46 +0530

    Time: 623 ms (5.03%), Space: 92.2 MB (18.97%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<int> prevsmall(vector<int>& heights)
+    {
+        vector <int>left;
+        stack <pair<int,int>>st;
+        for(int  i=0;i<heights.size();i++)
+        {
+            while(!st.empty() && heights[i]<=st.top().first)
+            {
+                st.pop();
+            }
+            if(st.empty())
+                left.push_back(-1);
+            else
+                left.push_back(st.top().second);
+            st.push({heights[i],i});
+        }
+        return left;
+    }
+    vector<int> nextsmall(vector<int>& heights)
+    {
+        vector <int>right;
+        stack <pair<int,int>>st;
+        for(int  i=heights.size()-1;i>=0;i--)
+        {
+            while(!st.empty() && heights[i]<=st.top().first)
+            {
+                st.pop();
+            }
+            if(st.empty())
+                right.push_back(heights.size());
+            
+            else
+                right.push_back(st.top().second);
+            
+            st.push({heights[i],i});
+        }
+        reverse(right.begin(),right.end());
+        
+        return right;
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        vector<int>t1=prevsmall(heights);
+        vector<int>t2=nextsmall(heights);
+        int area=INT_MIN;
+        for(int i=0;i<heights.size();i++){
+            int  a=0;
+            int l=heights[i];
+            int b=t2[i]-t1[i]-1;
+            if(t2[i]==-1){
+                   b=heights.size()-1;
+            }
+            a=l*b;
+            area=max(a,area);
+        }
+        return area;
+    }
+};
