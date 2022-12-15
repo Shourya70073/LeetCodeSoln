e22469d426eda75b268a847049a7c8f11aa406d1
Shourya Srivastava <shourya70073@gmail.com>
2022-12-15 10:17:42 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (51.73%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -4,13 +4,11 @@ public:
         if(s.length()==0){
             return 0;
         }
-        int k=s.length()-1;
-        int p=s.length()-1;
-        while(s[p]==32){
-            p--;
-        }
-        k=p;
         int d=0;
+        int k=s.length()-1;
+        while(s[k]==32){
+            k--;
+        }
         for(int i=k;i>=0;i--){
             if(s[i]==32)
                 break;
