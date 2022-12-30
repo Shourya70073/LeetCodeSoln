85dec7aa175009a74698dbfd63214fe7dfefddbb
Shourya Srivastava <shourya70073@gmail.com>
2022-12-30 15:47:43 +0530

    Added solution - LeetHub

--- Check if array is sorted - GFG/check-if-array-is-sorted.cpp
+++ Check if array is sorted - GFG/check-if-array-is-sorted.cpp
@@ -10,18 +10,12 @@ using namespace std;
 class Solution {
   public:
     bool arraySortedOrNot(int arr[], int n) {
-        if(n==1){
-            return true;
-        }
-        bool ans=arraySortedOrNot(arr+1,n-1);
-        if(ans){
-            if(arr[0]<=arr[1])
-                return true;
-            else
-              return false;
-        }
-        return ans;
-        
+       for(int i=0;i<n-1;i++){
+           if(arr[i]>arr[i+1]){
+               return false;
+           }
+       }
+       return true;
     }
 };
 
