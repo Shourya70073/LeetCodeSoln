680c60506f0115443eb0acf8a156fde8bb8cb6ea
Shourya Srivastava <shourya70073@gmail.com>
2023-01-14 12:48:16 +0530

    Time: 24 ms (79.26%), Space: 13.7 MB (73.77%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findOrder(int V, vector<vector<int>>& pre) {
+        vector<int>adj[V];
+        for(auto i:pre){
+          adj[i[1]].push_back(i[0]);
+        }
+        vector<int> indeg(V,0);
+        for(int i=0;i<V;i++){
+           for(auto j:adj[i]){
+               indeg[j]++;
+           }
+       }
+       
+       queue<int>qt;
+       for(int i=0;i<V;i++){
+           if(indeg[i]==0)
+             qt.push(i);
+       }
+       vector<int>topo;
+        vector<int>emp;
+       while(!qt.empty()){
+           int node=qt.front();
+           topo.push_back(node);
+           qt.pop();
+           for(auto i:adj[node]){
+               indeg[i]--;
+               if(indeg[i]==0)
+                 qt.push(i);
+           }
+       }
+       if(topo.size()==V)
+         return topo;
+       return emp;
+    }
+};
