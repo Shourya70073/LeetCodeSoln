9555ad37577bf19c0848df4bdc94aa98ac097345
Shourya Srivastava <shourya70073@gmail.com>
2023-03-26 19:42:20 +0530

    Time: 10 ms (18.06%), Space: 9.6 MB (79.61%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,18 +4,19 @@ public:
         int l=0;
         int h=nums.size()-1;
         while(l<=h){
-            int mid=l+(h-l)/2;
-           if(nums[mid]==target){
-               return mid;
-           }
+           int mid=(l+h)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            
             else if(target>nums[mid]){
                 l=mid+1;
             }
-            else{
-                h=mid-1;
+            else if(target<nums[mid]){
+                   h=mid-1;
             }
+            
         }
         return l;
-        
     }
 };
