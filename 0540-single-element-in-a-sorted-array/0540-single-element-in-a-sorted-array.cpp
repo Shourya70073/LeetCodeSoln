d46bff08e55f28a7b12653fb9f5e0b1e8b806c05
Shourya Srivastava <shourya70073@gmail.com>
2023-03-26 20:07:09 +0530

    Time: 32 ms (43.70%), Space: 22.2 MB (93.70%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     int singleNonDuplicate(vector<int>& arr) {
         int ans=0;
         for(int i=0;i<arr.size();i++){
-ans=ans^arr[i];
+            ans=ans^arr[i];
         }
         return ans;
     }
