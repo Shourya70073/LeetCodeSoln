bdf14a3eddd4ee382c8a0e4b6cd2e39e2aed3f23
Shourya Srivastava <shourya70073@gmail.com>
2023-03-26 19:07:17 +0530

    Time: 39 ms (65.93%), Space: 27.6 MB (18.49%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
+    int recurrBinary(vector<int>& nums,int l,int h, int target){
+        if(l>h ){
+         return -1;
+        }
+        
+       int mid=(l+h)/2;
+       if(nums[mid]==target){
+           return mid;
+       }
+       else if(target>nums[mid]){
+           return recurrBinary(nums,mid+1,h,target);
+       }
+       else{
+            return recurrBinary(nums,l,mid-1,target);
+       }
+    }
     int search(vector<int>& nums, int target) {
         int l=0;
         int h=nums.size()-1;
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(target>nums[mid]){
-                l=mid+1;
-            }
-            else if(target<nums[mid]) {
-       h=mid-1;
-            }
-        }
-        return -1;
+        return recurrBinary(nums,l,h,target);
     }
 };
