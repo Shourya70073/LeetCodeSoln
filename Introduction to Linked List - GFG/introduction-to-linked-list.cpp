fd9f945c2799ddd519c5fd2d127a2219314c09fd
Shourya Srivastava <shourya70073@gmail.com>
2023-04-12 20:41:05 +0530

    Added solution - LeetHub

--- Introduction to Linked List - GFG/introduction-to-linked-list.cpp
+++ Introduction to Linked List - GFG/introduction-to-linked-list.cpp
@@ -51,17 +51,11 @@ public:
 class Solution {
   public:
     Node* constructLL(vector<int>& arr) {
-       Node *head=nullptr;
-       Node *n=new Node(arr[0]);
-       head=n;
-       
-       Node *temp=n;
-       
+       Node *head=new Node(arr[0]);
+       Node *temp=head;
        for(int i=1;i<arr.size();i++){
-           Node *p=new Node(arr[i]);
-           temp->next=p;
+           temp->next=new Node(arr[i]);
            temp=temp->next;
-           
        }
        return head;
     }
