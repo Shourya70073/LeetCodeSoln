b845adc7d22983cc4d5205ed206dc6a602ca3941
Shourya Srivastava <shourya70073@gmail.com>
2023-07-02 22:16:54 +0530

    Added solution - LeetHub

---
+++ Introduction to Linked List - GFG/introduction-to-linked-list.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class Node { 
+    int data; 
+    Node next; 
+
+    Node() { data = 0; }
+    Node(int d) { data = d; }  //constructor to create a new node
+} 
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            Solution obj = new Solution();
+            Node ans = obj.constructLL(a);
+            while (ans != null) {
+                System.out.print(ans.data + " ");
+                ans = ans.next;
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node { 
+    int data; 
+    Node next; 
+
+    Node() { data = 0; }
+    Node(int d) { data = d; }  //constructor to create a new node
+} 
+*/
+class Solution {
+    static Node constructLL(int arr[]) {
+       Node n=new Node();
+       Node ans=n;
+       for(int i=0;i<arr.length;i++){
+           n.next=new Node(arr[i]);
+           n=n.next;
+       }
+       return ans.next;
+    }
+}
