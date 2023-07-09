7d39623b9b46fc32bb3afee18fafbe7db778cab8
Shourya Srivastava <shourya70073@gmail.com>
2023-07-09 10:37:39 +0530

    Time: 13 ms (16.29%), Space: 11.4 MB (7.46%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -2,33 +2,33 @@ class Solution {
 public:
     string simplifyPath(string path) {
         stack<string>st;
-      path.push_back('/');
-      string curr="";
-      for(auto i:path){
-          if(i=='/'){
-             if(curr==".."){
-                 if(!st.empty()){
-                     st.pop();
-                 }
-             }
-             
-            else if(curr!="" && curr!="."){
-                 st.push(curr);
-             }
-              curr="";
-             
-             }
-             else{
-                 curr=curr+i;
-             }
-      }
-             
-      string ans="";
-      while(!st.empty()){
-          string t=st.top();
-          ans="/" +t+ans;
-          st.pop();
-      }
-             return ans.size()==0?"/":ans;
+        path=path+'/';
+        string ans="";
+        string curr="";
+        for(auto i:path){
+            if(i=='/'){
+               
+                    if(curr==".."){
+                        if(!st.empty())
+                            st.pop();
+                    }
+                    else if(curr!="" && curr!="."){
+                        st.push(curr);
+                          
+                    }
+                 curr=""; 
+                
+                
+            }
+            else{
+               curr=curr+i; 
+            }
+        }
+        while(!st.empty()){
+            string t=st.top();
+            ans='/'+t+ans;
+            st.pop();
+        }
+        return ans.size()==0?"/":ans;
     }
 };
